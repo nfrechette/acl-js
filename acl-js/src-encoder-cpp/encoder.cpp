e4971651b1ca146883a7dbd67195f581afc51ad4
Nicholas Frechette <zeno490@gmail.com>
2021-05-04 21:50:13 -0400

    chore: update to ACL 2.0.0

--- acl-js/src-encoder-cpp/encoder.cpp
+++ acl-js/src-encoder-cpp/encoder.cpp
@@ -24,17 +24,17 @@
 
 #include "encoder.h"
 
-#include <acl/compression/animation_clip.h>
 #include <acl/compression/compress.h>
-#include <acl/compression/skeleton.h>
 #include <acl/compression/track_error.h>
-#include <acl/compression/utils.h>
 #include <acl/core/ansi_allocator.h>
-
-#include <acl/algorithm/uniformly_sampled/encoder.h>
-#include <acl/algorithm/uniformly_sampled/decoder.h>
+#include <acl/decompression/decompress.h>
 
 #include <cstdint>
+
+struct scalar_decompression_settings final : public acl::decompression_settings
+{
+	static constexpr bool is_track_type_supported(acl::track_type8 type) { return type == acl::track_type8::float1f; }
+};
 
 enum class sample_types
 {
@@ -52,8 +52,8 @@ struct qvv_track_description
 	double precision;
 	double shell_distance;
 
-	double constant_rotation_threshold;
-	double constant_translaton_threshold;
+	double constant_rotation_threshold_angle;
+	double constant_translation_threshold;
 	double constant_scale_threshold;
 };
 
@@ -79,55 +79,6 @@ struct track_error
 	double sample_time;
 };
 
-static acl::RigidSkeleton build_skeleton(const qvv_track_description* track_descriptions, uint32_t num_transforms, acl::IAllocator& allocator)
-{
-	acl::RigidBone* bones = acl::allocate_type_array<acl::RigidBone>(allocator, num_transforms);
-
-	for (uint32_t transform_index = 0; transform_index < num_transforms; ++transform_index)
-	{
-		const qvv_track_description& desc = track_descriptions[transform_index];
-
-		acl::RigidBone& bone = bones[transform_index];
-		bone.vertex_distance = desc.shell_distance;
-		bone.parent_index = desc.parent_index >= 0.0 ? static_cast<uint16_t>(desc.parent_index) : acl::k_invalid_bone_index;
-	}
-
-	acl::RigidSkeleton skeleton(allocator, bones, static_cast<uint16_t>(num_transforms));
-	acl::deallocate_type_array(allocator, bones, num_transforms);
-	return skeleton;
-}
-
-static acl::AnimationClip build_clip(const qvv_track_description* track_descriptions, const double* raw_data, const acl::RigidSkeleton& skeleton, uint32_t num_transforms, uint32_t num_samples_per_track, float sample_rate, acl::IAllocator& allocator)
-{
-	// Our raw data contains QVVs laid out: rotation0, translation0, scale0, rotation1, ...
-	acl::AnimationClip clip(allocator, skeleton, num_samples_per_track, sample_rate, acl::String());
-
-	for (uint32_t transform_index = 0; transform_index < num_transforms; ++transform_index)
-	{
-		acl::AnimatedBone& bone = clip.get_animated_bone(static_cast<uint16_t>(transform_index));
-
-		const qvv_track_description& desc = track_descriptions[transform_index];
-		bone.output_index = desc.output_index >= 0 ? static_cast<uint16_t>(desc.output_index) : acl::k_invalid_bone_index;
-
-		const uint32_t transform_raw_data_offset = transform_index * 10 * num_samples_per_track;
-		for (uint32_t sample_index = 0; sample_index < num_samples_per_track; ++sample_index)
-		{
-			const uint32_t sample_raw_data_offset = (sample_index * 10) + transform_raw_data_offset;
-
-			const rtm::quatd rotation = rtm::quat_normalize(rtm::quat_load(raw_data + sample_raw_data_offset + 0));
-			bone.rotation_track.set_sample(sample_index, rotation);
-
-			const rtm::vector4d translation = rtm::vector_load3(raw_data + sample_raw_data_offset + 4);
-			bone.translation_track.set_sample(sample_index, translation);
-
-			const rtm::vector4d scale = rtm::vector_load3(raw_data + sample_raw_data_offset + 7);
-			bone.scale_track.set_sample(sample_index, scale);
-		}
-	}
-
-	return clip;
-}
-
 static int compress_transforms(unsigned char* metadata, size_t metadata_size,
 	unsigned char* raw_data, size_t raw_data_size)
 {
@@ -148,40 +99,67 @@ static int compress_transforms(unsigned char* metadata, size_t metadata_size,
 	const double* raw_data_ = reinterpret_cast<const double*>(raw_data);
 	const qvv_track_description* track_descriptions = reinterpret_cast<const qvv_track_description*>(metadata_ + 4);
 
-	acl::ANSIAllocator allocator;
+	acl::ansi_allocator allocator;
 
-	const acl::RigidSkeleton skeleton = build_skeleton(track_descriptions, num_tracks, allocator);
-	const acl::AnimationClip clip = build_clip(track_descriptions, raw_data_, skeleton, num_tracks, num_samples_per_track, sample_rate, allocator);
+	acl::track_array_qvvf tracks(allocator, num_tracks);
+	for (uint32_t track_index = 0; track_index < num_tracks; ++track_index)
+	{
+		const qvv_track_description& js_desc = track_descriptions[track_index];
+
+		acl::track_desc_transformf desc;
+		desc.output_index = js_desc.output_index >= 0.0 ? static_cast<uint32_t>(js_desc.output_index) : acl::k_invalid_track_index;
+		desc.parent_index = js_desc.parent_index >= 0.0 ? static_cast<uint32_t>(js_desc.parent_index) : acl::k_invalid_track_index;
+		desc.precision = static_cast<float>(js_desc.precision);
+		desc.shell_distance = static_cast<float>(js_desc.shell_distance);
+		desc.constant_rotation_threshold_angle = static_cast<float>(js_desc.constant_rotation_threshold_angle);
+		desc.constant_translation_threshold = static_cast<float>(js_desc.constant_translation_threshold);
+		desc.constant_scale_threshold = static_cast<float>(js_desc.constant_scale_threshold);
+
+		acl::track_qvvf track = acl::track_qvvf::make_reserve(desc, allocator, num_samples_per_track, sample_rate);
+
+		const uint32_t transform_raw_data_offset = track_index * 10 * num_samples_per_track;
+		for (uint32_t sample_index = 0; sample_index < num_samples_per_track; ++sample_index)
+		{
+			const uint32_t sample_raw_data_offset = (sample_index * 10) + transform_raw_data_offset;
+
+			const rtm::quatd rotation = rtm::quat_load(raw_data_ + sample_raw_data_offset + 0);
+			const rtm::vector4d translation = rtm::vector_load3(raw_data_ + sample_raw_data_offset + 4);
+			const rtm::vector4d scale = rtm::vector_load3(raw_data_ + sample_raw_data_offset + 7);
+
+			track[sample_index] = rtm::qvv_set(rtm::quat_normalize(rtm::quat_cast(rotation)), rtm::vector_cast(translation), rtm::vector_cast(scale));
+		}
+
+		tracks[track_index] = std::move(track);
+	}
+
+	acl::compression_settings settings = acl::get_default_compression_settings();
 
 	acl::qvvf_transform_error_metric error_metric;
-
-	acl::CompressionSettings settings = acl::get_default_compression_settings();
 	settings.error_metric = &error_metric;
 
-	settings.constant_rotation_threshold_angle = rtm::radians(static_cast<float>(track_descriptions->constant_rotation_threshold));
-	settings.constant_translation_threshold = static_cast<float>(track_descriptions->constant_translaton_threshold);
-	settings.constant_scale_threshold = static_cast<float>(track_descriptions->constant_scale_threshold);
-	settings.error_threshold = static_cast<float>(track_descriptions->precision);
+	acl::compressed_tracks* compressed_tracks = nullptr;
+	acl::output_stats stats;
+	const acl::error_result result = acl::compress_track_list(allocator, tracks, settings, compressed_tracks, stats);
 
-	acl::CompressedClip* compressed_clip = nullptr;
-
-	acl::OutputStats stats;
-	const acl::ErrorResult result = acl::uniformly_sampled::compress_clip(allocator, clip, settings, compressed_clip, stats);
 	if (result.any())
 		return -3;	// Compression failed
 
-	const uint32_t compressed_size = compressed_clip->get_size();
+	const uint32_t compressed_size = compressed_tracks->get_size();
 	if (raw_data_size < compressed_size)
 	{
-		allocator.deallocate(compressed_clip, compressed_size);
+		allocator.deallocate(compressed_tracks, compressed_size);
 		return -4;	// Raw data buffer is too small
 	}
 
-	acl::uniformly_sampled::DecompressionContext<acl::uniformly_sampled::DebugDecompressionSettings> context;
-	context.initialize(*compressed_clip);
+	acl::decompression_context<acl::default_transform_decompression_settings> context;
+	if (!context.initialize(*compressed_tracks))
+	{
+		allocator.deallocate(compressed_tracks, compressed_size);
+		return -5;	// Failed to initialize our decompression context
+	}
 
-	const acl::BoneError bone_error = acl::calculate_error_between_clips(allocator, error_metric, clip, context);
-	const track_error err = { double(bone_error.index), double(bone_error.error), double(bone_error.sample_time) };
+	const acl::track_error track_err = acl::calculate_compression_error(allocator, tracks, context, error_metric);
+	const track_error err = { double(track_err.index), double(track_err.error), double(track_err.sample_time) };
 
 	// Copy our error stats into the metadata buffer, it is no longer needed
 	// and it should be large enough
@@ -189,9 +167,9 @@ static int compress_transforms(unsigned char* metadata, size_t metadata_size,
 
 	// Copy our compressed clip back into the raw data buffer, it is no longer needed
 	// and it should be large enough.
-	std::memcpy(raw_data, compressed_clip, compressed_size);
+	std::memcpy(raw_data, compressed_tracks, compressed_size);
 
-	allocator.deallocate(compressed_clip, compressed_size);
+	allocator.deallocate(compressed_tracks, compressed_size);
 	return compressed_size;
 }
 
@@ -215,7 +193,7 @@ static int compress_scalars(unsigned char* metadata, size_t metadata_size,
 	const double* raw_data_ = reinterpret_cast<const double*>(raw_data);
 	const scalar_track_description* track_descriptions = reinterpret_cast<const scalar_track_description*>(metadata_ + 4);
 
-	acl::ANSIAllocator allocator;
+	acl::ansi_allocator allocator;
 
 	acl::track_array tracks(allocator, num_tracks);
 	for (uint32_t track_index = 0; track_index < num_tracks; ++track_index)
@@ -243,8 +221,8 @@ static int compress_scalars(unsigned char* metadata, size_t metadata_size,
 	acl::compression_settings settings;
 
 	acl::compressed_tracks* compressed_tracks = nullptr;
-	acl::OutputStats stats;
-	const acl::ErrorResult result = acl::compress_track_list(allocator, tracks, settings, compressed_tracks, stats);
+	acl::output_stats stats;
+	const acl::error_result result = acl::compress_track_list(allocator, tracks, settings, compressed_tracks, stats);
 
 	if (result.any())
 		return -3;	// Compression failed
@@ -256,8 +234,12 @@ static int compress_scalars(unsigned char* metadata, size_t metadata_size,
 		return -4;	// Raw dara buffer is too small
 	}
 
-	acl::decompression_context<acl::debug_decompression_settings> context;
-	context.initialize(*compressed_tracks);
+	acl::decompression_context<scalar_decompression_settings> context;
+	if (!context.initialize(*compressed_tracks))
+	{
+		allocator.deallocate(compressed_tracks, compressed_size);
+		return -5;	// Failed to initialize our decompression context
+	}
 
 	const acl::track_error track_err = acl::calculate_compression_error(allocator, tracks, context);
 	const track_error err = { double(track_err.index), double(track_err.error), double(track_err.sample_time) };
